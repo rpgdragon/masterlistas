5790d08d5ba0afbce887628e4ad546ef6153277d
jmcastellano <jmcastellano@jmcastellano.eu>
2017-11-01 00:26:01 +0100

    Mostrar/ocultar caracteres en campo de contraseña

--- app/src/main/java/org/example/masterlistas/InicioSesionActivity.java
+++ app/src/main/java/org/example/masterlistas/InicioSesionActivity.java
@@ -2,8 +2,10 @@ package org.example.masterlistas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class InicioSesionActivity extends AppCompatActivity {
@@ -18,5 +20,16 @@ public class InicioSesionActivity extends AppCompatActivity {
         CheckBox recordarme= (CheckBox) findViewById(R.id.recordarme);
         String s = "Recordar datos de usuario: " + (recordarme.isChecked() ? "Sí" : "No");
         Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+
+    public void mostrarContraseña(View v) {
+        EditText contraseña = (EditText) findViewById(R.id.contraseña);
+        CheckBox mostrar = (CheckBox) findViewById(R.id.mostrar_contraseña);
+        if (mostrar.isChecked()) {
+            contraseña.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+        }
+        else {
+            contraseña.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        }
     }
 }
