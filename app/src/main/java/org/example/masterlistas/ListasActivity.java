a8c1e3761c400b64853844005aa3afc4dc12d03f
jmcastellano <jmcastellano@jmcastellano.eu>
2017-11-01 23:37:20 +0100

    Añadir un Navigation Drawer en MasterListas.

--- app/src/main/java/org/example/masterlistas/ListasActivity.java
+++ app/src/main/java/org/example/masterlistas/ListasActivity.java
@@ -1,18 +1,24 @@
 package org.example.masterlistas;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListasActivity extends AppCompatActivity {
+public class ListasActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private RecyclerView recycler;
     private RecyclerView.Adapter adapter;
@@ -47,5 +53,42 @@ public class ListasActivity extends AppCompatActivity {
                 Toast.makeText(ListasActivity.this, "" + position, Toast.LENGTH_SHORT).show();
             }
         }) );
+        Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        // Navigation Drawer
+        DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string. drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById( R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_1) {
+            // …
+        }
+        else if (id == R.id.nav_2) {
+            // …
+        }
+        else if (id == R.id.nav_3) {
+            // …
+        } // …
+        DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START); return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
